c185158fde4d8535b71be22f752f03ded3ef3ad9
Brandon Valenzuela <brandonvalenzuela48@gmail.com>
2023-04-28 04:04:55 -0700

    Primer commit

---
+++ EccobanckAdmin/EccobanckAdmin/EccobanckAdmin/View/Mainmenu.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EccobanckAdmin.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Mainmenu : ContentPage
+    {
+        public Mainmenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
